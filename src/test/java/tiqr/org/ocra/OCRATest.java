124561361386a9fa38c2b40591add6949050651b
Okke Harsta <oharsta@zilverline.com>
2022-04-04 12:30:16 +0200

    OCRA test fails

--- src/test/java/tiqr/org/ocra/OCRATest.java
+++ src/test/java/tiqr/org/ocra/OCRATest.java
@@ -1,11 +1,12 @@
 package tiqr.org.ocra;
 
-import tiqr.org.secure.Challenge;
-import tiqr.org.secure.OCRA;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.Test;
+import tiqr.org.secure.Challenge;
+import tiqr.org.secure.OCRA;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +15,19 @@ class OCRATest {
 
     private String hexString(String s) {
         return Hex.encodeHexString(s.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String sessionKey = "3132333435363738393031323334353637383930313233343536373839303132";
+
+    private List<Input> inputList = List.of(
+            new Input("0000000000", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "174452")
+    );
+
+    @Test
+    void validateInput() {
+        inputList.forEach(input -> {
+            assertEquals(input.expectedOcra, OCRA.generateOCRA(input.sharedSecret, input.challenge, sessionKey));
+        });
     }
 
     @Test
@@ -34,6 +48,18 @@ class OCRATest {
                 hexString("timestamp"));
 
         assertEquals(result, other);
+    }
+
+    private class Input {
+        public String challenge;
+        public String sharedSecret;
+        public String expectedOcra;
+
+        public Input(String challenge, String sharedSecret, String expectedOcra) {
+            this.challenge = challenge;
+            this.sharedSecret = sharedSecret;
+            this.expectedOcra = expectedOcra;
+        }
     }
 
 
