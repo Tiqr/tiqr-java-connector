b63d59639825e1248c01de6f7f55d88d19da232a
Okke Harsta <oharsta@zilverline.com>
2022-06-07 11:20:54 +0200

    Removed not used EnrollmentStatusses

--- src/main/java/tiqr/org/model/EnrollmentStatus.java
+++ src/main/java/tiqr/org/model/EnrollmentStatus.java
@@ -4,8 +4,6 @@ public enum EnrollmentStatus {
 
     INITIALIZED, // An enrollment session has begun
     RETRIEVED,   // The device has retrieved the metadata
-    PROCESSED,   // The device has sent back a secret
-    FINALIZED,   // The application has stored the secret
-    VALIDATED;   // A first successful authentication was performed
+    PROCESSED   // The device has sent back a secret
 
 }
