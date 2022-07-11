6daa22d7e4063d3dc18c980be00167f12b32c198
Okke Harsta <oharsta@zilverline.com>
2022-07-11 09:53:50 +0200

    Added test for cipher

--- src/test/java/tiqr/org/secure/SecretCipherTest.java
+++ src/test/java/tiqr/org/secure/SecretCipherTest.java
@@ -13,13 +13,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SecretCipherTest {
 
-    private final SecretCipher secretCipher = new SecretCipher("1");
+    private final SecretCipher secretCipher = new SecretCipher(UUID.randomUUID().toString());
 
     @Test
-    void encrypt() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    void encrypt() {
         String sharedSecret = UUID.randomUUID().toString();
         String encrypted = secretCipher.encrypt(sharedSecret);
         String decrypted = secretCipher.decrypt(encrypted);
+        assertEquals(sharedSecret, decrypted);
+    }
+
+    @Test
+    void ensureThreadConsistency() {
+        String secret = UUID.randomUUID().toString();
+        SecretCipher firstSecretCipher = new SecretCipher(secret);
+        SecretCipher secondSecretCipher = new SecretCipher(secret);
+
+        String sharedSecret = UUID.randomUUID().toString();
+        String encrypted = firstSecretCipher.encrypt(sharedSecret);
+        String decrypted = secondSecretCipher.decrypt(encrypted);
         assertEquals(sharedSecret, decrypted);
     }
 
